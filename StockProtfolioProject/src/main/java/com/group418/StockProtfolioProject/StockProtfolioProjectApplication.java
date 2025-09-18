0d9ba023f30431ccc3bffc85b02aee3cae4deab6
Szymon Bedeniczuk <01158593@pw.edu.pl>
2025-09-18 16:49:39 +0200

    added the spring project to the directory

---
+++ StockProtfolioProject/src/main/java/com/group418/StockProtfolioProject/StockProtfolioProjectApplication.java
@@ -0,0 +1,13 @@
+package com.group418.StockProtfolioProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockProtfolioProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockProtfolioProjectApplication.class, args);
+	}
+
+}
---
+++ StockProtfolioProject/src/test/java/com/group418/StockProtfolioProject/StockProtfolioProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group418.StockProtfolioProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockProtfolioProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
